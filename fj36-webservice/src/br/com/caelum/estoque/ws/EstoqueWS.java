5a7c54e21fd2232fdbaaa27e08c96ed8031452d5
Your Name <marcospsz@gmail.com>
2018-03-07 21:10:43 -0300

    class 2 - before coffee

--- fj36-webservice/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ fj36-webservice/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -1,13 +1,18 @@
 package br.com.caelum.estoque.ws;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
-@WebService
+@WebService(targetNamespace="http://caelum.com.br/estoquews/v1")
 @Stateless
 public class EstoqueWS {
 	
@@ -21,10 +26,34 @@ public class EstoqueWS {
 		repositorio.put("LOG",	new	ItemEstoque("LOG",	4));
 		repositorio.put("WEB",	new	ItemEstoque("WEB",	1));
 		repositorio.put("ARQ",	new	ItemEstoque("ARQ",	2));
-	}
+	}	
 	
-	@WebMethod
-	public	ItemEstoque	getQuantidade(String codigo)	{		
-					return	repositorio.get(codigo);
-	}
+	
+	@WebMethod(operationName="ItensPeloCodigo")
+	@WebResult(name="ItemEstoque")	
+	public List<ItemEstoque> getItemEstoque(@WebParam(name = "codigo") List<String> codigos,
+	                                        @WebParam(name = "tokenUsuario", header = true) String token) {		
+		
+	    if(token==null || !token.equals("token123")){
+	    	throw new AutorizacaoException("Nao autorizado") ;
+	    }
+	                                        	
+		List<ItemEstoque> itens = new ArrayList<>() ;
+		
+		if(codigos == null || codigos.isEmpty()){
+			
+			return itens;
+					
+		}
+		
+		for(String codigo : codigos){
+			
+			if(repositorio.containsKey(codigo)){
+				
+				itens.add(repositorio.get(codigo));
+			}			
+		}
+		
+		return itens ;
+	}		
 }
